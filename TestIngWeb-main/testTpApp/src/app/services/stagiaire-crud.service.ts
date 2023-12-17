8ceb962e2811cf0b9033dd8313ff4a5bb0b15fc8
unknown <ikhlas>
2023-12-18 00:49:27 +0100

    test

---
+++ TestIngWeb-main/testTpApp/src/app/services/stagiaire-crud.service.ts
@@ -0,0 +1,33 @@
+// @ts-ignore
+import { Injectable } from '@angular/core';
+// @ts-ignore
+import {HttpClient} from "@angular/common/http";
+// @ts-ignore
+import {Observable} from "rxjs/internal/Observable";
+import {Stagiaire} from "../stagiaire";
+
+// @ts-ignore
+@Injectable({
+  providedIn: 'root'
+})
+export class StagiaireCrudService {
+
+  private url="http://localhost:8085/stagiaires";
+
+  constructor(private  httpClient:HttpClient) { }
+  getAllStg():Observable<Stagiaire[]>{
+    return this.httpClient.get<Stagiaire[]>(this.url);
+  }
+  AddStg(stagiaire: Stagiaire):Observable<Stagiaire>{
+    return this.httpClient.post<Stagiaire>(this.url,stagiaire);
+  }
+  updateStg(id:number, stagiaire:Stagiaire):Observable<Stagiaire>{
+    return this.httpClient.put<Stagiaire>(this.url+"/"+id,stagiaire);
+  }
+  deleteStg(id:number):Observable<string>{
+    return this.httpClient.delete<string>(this.url+"/"+id);
+  }
+  getStg(id:number):Observable<Stagiaire>{
+    return this.httpClient.get<Stagiaire>(this.url+"/"+id);
+  }
+}
